b6c8f07b65155cb9b752a496bd53cfc5205caeb4
Nobuyasu Tomura <ntomura92@gmail.com>
2020-04-19 16:15:33 +0900

    Add asm_printf(fmt, ...)

--- tcc_main.c
+++ tcc_main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+
+void errorf(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void asm_printf(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stdout, fmt, ap);
+  fprintf(stdout, "\n");
+}
 
 void gen_asm() {
-    puts(".intel_syntax noprefix");
-    puts(".global main");
-    puts("main:");
-    printf("  mov rax, 4\n");
-    printf("  ret\n");
+  asm_printf(".intel_syntax noprefix");
+  asm_printf(".global main");
+  asm_printf("main:");
+  asm_printf("  mov rax, 4");
+  asm_printf("  ret");
 }
 
 #define OPT_GEN_ASM "-S\0"
@@ -15,12 +31,10 @@ void gen_asm() {
 
 int main(int argc, char const** argv)
 {
-    // printf("%s, %ld\n", OPT_GEN_ASM, OPT_GEN_ASM_STRLEN);
-    if (argc >= 2 && strncmp(argv[1], OPT_GEN_ASM, OPT_GEN_ASM_STRLEN) == 0) {
-        gen_asm();
-        return 0;
-    }
+  if (argc >= 2 && strncmp(argv[1], OPT_GEN_ASM, OPT_GEN_ASM_STRLEN) == 0) {
+    gen_asm();
+    return 0;
+  }
 
-    puts("usage: tcc -S");
-    // gen_asm();
+  puts("usage: tcc -S");
 }
